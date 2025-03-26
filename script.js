230a3083be56e843541e8b638fda6a6baa0652f7
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-03-26 17:24:27 -0300

    ajustes aqui e ali, animação da foto e arrumei o grid

--- script.js
+++ script.js
@@ -2,15 +2,28 @@
 const sidebarToggle = document.getElementById('sidebar-toggle'); // botão para abrir o menu
 const sidebarMenu = document.getElementById('sidebar-menu'); // menu lateral
 const overlay = document.getElementById('overlay'); // o overlay
+const menuSection = document.querySelectorAll('.menu-section'); // 
 
-// Adiciona evento de clique no botão para abrir o menu
+// para abrir o menu
 sidebarToggle.addEventListener('click', () => {
     sidebarMenu.classList.add('open');
     overlay.classList.add('active');
 });
 
-// Adiciona evento de clique no overlay para fechar o menu
-overlay.addEventListener('click', () => {
+// para fechar o menu
+function fecharMenu() {
     sidebarMenu.classList.remove('open');
     overlay.classList.remove('active');
+}
+
+overlay.addEventListener('click', fecharMenu); // quando o usuário clicar fora do menu lateral
+
+menuSection.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault(); // Impede a rolagem imediata
+            setTimeout(() => {
+                fecharMenu();
+                window.location.href = item.getAttribute("href"); // procura a section correspondente
+            }, 100); // atraso de 1s
+    });
 });
